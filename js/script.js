4b35f61fac4c241cf582758470efebb8c6eb8722
Pietro Nencini <nencinipietro2006@gmail.com>
2024-11-01 12:16:14 +0100

    Added lost files (everything)

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showHiddenForm() {
+    document.getElementById('hidden_form').style.display = 'block';
+    document.getElementById('multi').value = 1;
+    
+}
+
+function hideHiddenForm() {
+    document.getElementById('hidden_form').style.display = 'none';
+}
---
+++ php/purchaseManager.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizio 30 - PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    
+    <?php
+        const CURVA_COST = 30;
+        const CENTRALE_COST = 80;
+        const VIP_COST = 120;
+
+
+
+
+
+
+
+        // todo Dati inseriti dall'utente
+        // todo Data dell'acquisto 
+        // todo Numero di biglietti 
+        // todo Data dell'acquisto 
+        // todo Data dell'acquisto 
+
+
+    ?> 
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
