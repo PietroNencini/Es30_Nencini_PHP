e10596cc502829438085d4b9cf2821be67d6e949
Pietro Nencini <nencinipietro2006@gmail.com>
2024-11-01 15:37:10 +0100

    Almost completed

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ function showHiddenForm() {
 function hideHiddenForm() {
     document.getElementById('hidden_form').style.display = 'none';
 }
+
+function addExtraInputs() {
+    let extraInput_container = document.getElementById('extra_cfs');
+    let extraInput_num = document.getElementById('num').value;  //todo L'input in html fa sì che questo valore sia sempre compreso tra 0 e 4 (inclusi)
+    extraInput_container.innerHTML = "";                        //todo pulisce eventuali input di codice fiscali già presenti per rimpiazzarli con quelli nuovi in numero adeguato all'input inserito
+    for(let i = 2; i <= extraInput_num; i++) {
+        let current_id = "fiscal_code_add_" + i;
+        let new_cf_input = document.createElement("input");
+        new_cf_input.setAttribute("type", "text");
+        new_cf_input.setAttribute("name", current_id);
+        new_cf_input.setAttribute("id", current_id);
+        new_cf_input.setAttribute("class", "form-control");
+        new_cf_input.setAttribute("maxlength", "16");
+        new_cf_input.setAttribute("required", true);
+        new_cf_input.setAttribute("placeholder", "CF AGGIUNTIVO...");
+        new_cf_input.setAttribute("value", "");
+        extraInput_container.appendChild(new_cf_input);
+    }
+}
--- php/purchaseManager.php
+++ php/purchaseManager.php
@@ -13,21 +13,76 @@
         const CURVA_COST = 30;
         const CENTRALE_COST = 80;
         const VIP_COST = 120;
+        $available_coupons = ['FIRENZE5' => 0.05];
+        $additional_fc = [];
+        //* dati utente
+        $name = $_GET['user_name'];
+        $surname = $_GET['user_surname'];
+        $fc = $_GET['fiscal_code'];           // Per semplicità non controlliamo che il codice fiscale sia valido in base alla sua espressione regolare
+        $sector = $_GET['sectorChoice'];
+        $coupon = $_GET['coupon'];
 
+        //* data e ora acquisto
+        $purchase_time = date('Y-m-d H:i:s');
 
+        //* numero biglietti acquistati (massimo 4 acquistabili per ogni utente)   
+        $num_ticket_choice = $_GET['single_multi'];
+        $ticket_num = $num_ticket_choice == "s" ? 1 : $_GET['ticket_num'];
 
+        //*lista codici fiscali degli eventuali biglietti aggiuntivi (se presenti)
+        if($num_ticket_choice == "m")
+            for($i=2; $i <= $_GET['ticket_num']; $i++) {
+                $additional_fc[$i-2] = $_GET["fiscal_code_add_$i"];
+            }
 
+        $total_cost = $ticket_num;
+        switch($sector) {
+            case "curva":
+                $total_cost *= CURVA_COST;
+                break;
+            case "centrale":
+                $total_cost *= CENTRALE_COST;
+                break;
+            case "onore":
+                $total_cost *= VIP_COST;
+        }
 
-
-
-        // todo Dati inseriti dall'utente
-        // todo Data dell'acquisto 
-        // todo Numero di biglietti 
-        // todo Data dell'acquisto 
-        // todo Data dell'acquisto 
-
-
+        $new_total = in_array($coupon, $available_coupons) ? $total_cost * (1 - $available_coupons[$coupon]) : $total_cost;
     ?> 
+
+    <div id="price_list" class="w-50 mx-auto my-3 bg-body-tertiary rounded-4 p-3">
+        <h2> PREZZI BIGLIETTI </h2>
+        <ul style="list-style-type: none;" class="fs-5">
+            <li> CURVA : <?php echo CURVA_COST ?> € </li>
+            <li> TRIBUNA CENTRALE: <?php echo CENTRALE_COST ?>  €</li>
+            <li> TRIBUNA D'ONORE: <?php echo VIP_COST ?> € </li>
+        </ul>
+    </div>
+    <div id="riepilogo" class="w-75 mx-auto bg-body-secondary shadow-lg fs-5 rounded-4 p-3">
+        <h3> RIEPILOGO ACQUISTO BIGLIETTI </h3> <hr>
+        <p>Nome acquirente: <?php echo $name ?> </p>
+        <p>Cognome acquirente: <?php echo $surname ?> </p>
+        <p>Codice fiscale: <?php echo $fc ?> </p>
+        <p> Settore di prenotazione: <?php echo $sector ?> </p>
+        <p> Modalità di acquisto: <?php
+            if($num_ticket_choice == "s")
+                echo "Biglietto singolo";
+            else {
+                echo "Biglietto multiplo - $ticket_num persone";
+                echo "<br> Codice fiscale utenti aggiuntivi: <ul> <li>".implode(",", $additional_fc)."</li> </ul>";
+            }
+        ?></p>
+        <?php
+            if(in_array($coupon, $available_coupons))
+                echo "<p>Coupon utilizzato: $coupon - sconto del ". $available_coupons[$coupon] * 100 ."%</p>";
+            else
+                echo "<p> Buono sconto non inserito o non valido </p>";
+        ?>
+        <p> Preventivo totale: <?php echo $total_cost?> €</p>
+        <p> Costo effettivo (eventuali sconti applicati):<b> <?php echo $new_total ?>€</b></p>
+        <p> Data acquisto  <?php echo $purchase_time ?></p>
+    </div>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
